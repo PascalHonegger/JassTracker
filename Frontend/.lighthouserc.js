f951af0b7f3087ed25de143164653e2c18847ca5
Pascal Honegger <pascal.honegger@bluewin.ch>
2022-04-26 10:31:20 +0200

    feat(lhci): disable pwa in lighthouse

--- Frontend/.lighthouserc.js
+++ Frontend/.lighthouserc.js
@@ -2,9 +2,10 @@ module.exports = {
   ci: {
     collect: {
       settings: { chromeFlags: "--no-sandbox" },
+      staticDistDir: "./dist",
     },
     assert: {
-      preset: "lighthouse:recommended",
+      preset: "lighthouse:no-pwa",
     },
     upload: {
       target: "filesystem",
