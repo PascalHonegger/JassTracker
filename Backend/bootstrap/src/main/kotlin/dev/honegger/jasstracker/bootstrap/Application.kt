0a22019461138e4b803ac49aed14e5935cdcb15e
Jamie Maier <jamie.maier@ost.ch>
2022-05-07 17:07:08 +0200

    feat(authentication): added application.conf property getters JWTConfig and passwordHashConfig

--- Backend/bootstrap/src/main/kotlin/dev/honegger/jasstracker/bootstrap/Application.kt
+++ Backend/bootstrap/src/main/kotlin/dev/honegger/jasstracker/bootstrap/Application.kt
@@ -7,42 +7,59 @@ import dev.honegger.jasstracker.data.repositories.*
 import dev.honegger.jasstracker.domain.services.*
 import dev.honegger.jasstracker.api.endpoints.*
 import dev.honegger.jasstracker.bootstrap.plugins.configureAuthentication
+import dev.honegger.jasstracker.security.Argon2HashConfig
+import dev.honegger.jasstracker.security.Argon2PasswordHashService
+import dev.honegger.jasstracker.security.JwtConfig
+import dev.honegger.jasstracker.security.JwtTokenService
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.auth.*
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
-    initializeDatabase()
-    configureHTTP()
-    configureStaticRouting()
-    configureAuthentication()
+    environment.config.apply {
+        val jwtConfig = JwtConfig(secret = property("jwt.secret").getString(),
+            issuer = property("jwt.issuer").getString(),
+            audience = property("jwt.audience").getString(),
+            realm = property("jwt.realm").getString(),
+            expiryTime = property("jwt.expiryTime").getString())
+        val argon2HashConfig =
+            Argon2HashConfig(iterations = property("hash.iterations").toString(),
+                memory = property("hash.memory").toString(),
+                parallelization = property("hash.parallelization").toString())
 
-    // Could be provided by a DI framework like Koin
-    val gameRepository = GameRepositoryImpl()
-    val tableRepository = TableRepositoryImpl(gameRepository)
-    val playerRepository = PlayerRepositoryImpl()
-    val contractRepository = ContractRepositoryImpl()
+        initializeDatabase()
+        configureHTTP()
+        configureStaticRouting()
+        configureAuthentication(jwtConfig)
 
-    val gameService = GameServiceImpl(gameRepository, playerRepository)
-    val tableService = TableServiceImpl(tableRepository)
-    val roundRepository = RoundRepositoryImpl()
-    val roundService = RoundServiceImpl(roundRepository, tableRepository)
-    val contractService = ContractServiceImpl(contractRepository)
-    val playerService = PlayerServiceImpl(playerRepository)
+        // Could be provided by a DI framework like Koin
+        val gameRepository = GameRepositoryImpl()
+        val tableRepository = TableRepositoryImpl(gameRepository)
+        val playerRepository = PlayerRepositoryImpl()
+        val contractRepository = ContractRepositoryImpl()
 
-    routing {
-        authenticate {
-            configureGameEndpoints(gameService)
-            configureTableEndpoints(tableService)
-            configureContractEndpoints(contractService)
-            configurePlayerEndpoints(playerService)
-            configureRoundEndpoints(roundService)
+        val authTokenService = JwtTokenService(jwtConfig)
+        val passwordHashService = Argon2PasswordHashService(argon2HashConfig)
+        val gameService = GameServiceImpl(gameRepository, playerRepository)
+        val tableService = TableServiceImpl(tableRepository)
+        val roundRepository = RoundRepositoryImpl()
+        val roundService = RoundServiceImpl(roundRepository, tableRepository)
+        val contractService = ContractServiceImpl(contractRepository)
+        val playerService = PlayerServiceImpl(playerRepository, passwordHashService)
+
+        configureAuthenticationEndpoints(authTokenService)
+
+        routing {
+            authenticate {
+                configureGameEndpoints(gameService)
+                configureTableEndpoints(tableService)
+                configureContractEndpoints(contractService)
+                configurePlayerEndpoints(playerService)
+                configureRoundEndpoints(roundService)
+            }
         }
     }
-
-
 }
--- Backend/bootstrap/src/main/kotlin/dev/honegger/jasstracker/bootstrap/plugins/Authentication.kt
+++ Backend/bootstrap/src/main/kotlin/dev/honegger/jasstracker/bootstrap/plugins/Authentication.kt
@@ -2,32 +2,31 @@ package dev.honegger.jasstracker.bootstrap.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import dev.honegger.jasstracker.security.JwtConfig
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
 
-fun Application.configureAuthentication () {
-    environment.config.apply {
-        install(Authentication) {
-            jwt {
-                realm = property("jwt.realm").getString()
-                verifier(JWT
-                    .require(Algorithm.HMAC256(property("jwt.secret").getString()))
-                    .withAudience(property("jwt.audience").getString())
-                    .withIssuer(property("jwt.issuer").getString())
-                    .build())
-                validate { credential ->
-                    if (credential.payload.getClaim("username").asString() != "") {
-                        JWTPrincipal(credential.payload)
-                    } else {
-                        null
-                    }
+fun Application.configureAuthentication(jwtConfig: JwtConfig) {
+    install(Authentication) {
+        jwt {
+            realm = jwtConfig.realm
+            verifier(JWT
+                .require(Algorithm.HMAC256(jwtConfig.secret))
+                .withAudience(jwtConfig.audience)
+                .withIssuer(jwtConfig.issuer)
+                .build())
+            validate { credential ->
+                if (credential.payload.getClaim("username").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
                 }
-                challenge { defaultScheme, realm ->
-                    call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
-                }
+            }
+            challenge { defaultScheme, realm ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
             }
         }
     }
