2aab69ab37ea02e95e741484604f026b285c5218
Marcel Joss <marcel.joss@ost.ch>
2022-05-07 09:15:49 +0200

    fix(frontend): fix white on white text in light theme

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -25,7 +25,7 @@ nav a.router-link-exact-active {
 </style>
 <template>
   <div>
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
+    <nav class="border-gray-200 px-2 sm:px-4 py-2.5 bg-gray-800">
       <div
         class="container flex flex-wrap justify-between items-center mx-auto"
       >
