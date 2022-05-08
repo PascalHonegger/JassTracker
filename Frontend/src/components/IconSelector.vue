038eca268fe5fe72337821953a3f1d8da46539c9
Marcel Joss <marcel.joss@ost.ch>
2022-05-08 16:33:10 +0200

    feat(frontend): allow toggling mobile navigation

--- Frontend/src/components/IconSelector.vue
+++ Frontend/src/components/IconSelector.vue
@@ -22,4 +22,14 @@ const props = defineProps<{
       d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
     />
   </svg>
+  <svg
+    v-else-if="props.icon === 'hamburger'"
+    fill="#ffffff"
+    viewBox="0 0 100 80"
+    class="w-6 h-6"
+  >
+    <rect width="100" height="20"></rect>
+    <rect y="30" width="100" height="20"></rect>
+    <rect y="60" width="100" height="20"></rect>
+  </svg>
 </template>
