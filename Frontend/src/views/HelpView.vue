4ae29a4ba0d57d52d10e290021a3821c5b615d59
David Kalchofner <david.kalchofner@ost.ch>
2022-05-11 14:37:16 +0200

    feat(instructions): add images and format text

--- Frontend/src/views/HelpView.vue
+++ Frontend/src/views/HelpView.vue
@@ -91,93 +91,97 @@
     <h3 class="text-3xl font-normal leading-normal mt-4">
       Login / Registierung
     </h3>
-    <ul>
-      <li>
+    <div class="flex py-2">
+      <p class="text-left">
         Der JassTracker kann entweder als Gast oder mit einem erstellen Benutzer
         Profil benutzt werden
-      </li>
-      <li>
+        <br />
         Um einen Benutzer Account zu erhalten, muss man sich zuerst registrieren
         unter <a href="/">Registrieren</a>. Danach kann man sich mit den
         Benutzer Informationen einloggen und den JassTracker im vollen Umfang
         verwenden
-      </li>
-      <li>
+        <br />
         Wenn man sich keinen Account erstellen möchte, oder den JassTracker mal
         ausprobieren möchte, kann man sich "als Gast" anmelden. Man kann die
         Kern-Funktionalitaten der Applikation ausprobieren und sehen, ob mann
         einen Account erstellen möchte.
-      </li>
-    </ul>
+      </p>
+      <img
+        class="max-w-[50%] pl-4"
+        src="../assets/helpcenter/login.webp"
+        alt="Login Screen"
+      />
+    </div>
     <h3 class="text-3xl font-normal leading-normal mt-4">Tische</h3>
-    <ul>
-      <li>
+    <div class="flex py-2">
+      <img
+        class="max-w-[50%] pr-4"
+        src="../assets/helpcenter/tables.webp"
+        alt="Tisch Übersicht"
+      />
+      <p class="text-left">
         Sobald man sich eingeloggt hat (entweder mit dem eigenen Benutzer
         Account oder als Gast), kann man neue Tische erstellen
-      </li>
-      <li>
+        <br />
         Tische enthalten immer 4 Personen, somit hat man alle Spiele die man mit
         diesen Personen spielt, immer an einem Ort und sorgt für eine
         übersichtliche Übersicht.
-      </li>
-      <li>
+        <br />
         Ein Tisch kann zu dem einen Namen haben, z.B. "Familientisch", wo man
         immer mit seinen Familienmitgliedern spielt
-      </li>
-    </ul>
+      </p>
+    </div>
     <h3 class="text-3xl font-normal leading-normal mt-4">Spiele</h3>
-    <ul>
-      <li>
+    <div class="flex py-2">
+      <p class="text-left">
         Spiele sind immer in einem Tisch enthalten. Bei der Erstellung eines
         Tisches wird automatisch das erste Spiel erstellt.
-      </li>
-      <li>
+        <br />
         Spiele können mit "Spiel abschliessen" entweder frühzeitig oder sobald
         alle Möglichkeiten gespielt wurden abgeschlossen werden.
-      </li>
-      <li>
+        <br />
         Ein neues Spiel kann via "Neues Spiel erstellen" eröffnet werden, man
         erhaält die Option, die Teams neu zu bilden bei Wunsch.
-      </li>
-      <li>
+        <br />
         Beim Klick auf ein Spiel in der unteren Box Ansicht, kommt man auf die
         Detail Seite wo man die Möglichkeit hat, ein Spiel zu löschen. Nur
         abgeschlossene Spiele können geloscht werden.
-      </li>
-      <li>
+        <br />
         In dieser Detail Ansicht, sieht man zusätzlichen aktuelle
         Spielstatistiken wie aktuelle Siegeschancen pro Team oder
         Durchschnittliche Punktzahl etc.
-      </li>
-      <li>
+        <br />
         Anstatt z.B. 157 - 15 zu rechnen und dann 142 einzugeben, kann auch
         direkt "-15" eigetragen werden. Eingaben im Spiel können angepasst und
         auch gelöscht werden. Es werden nur Zahlen und "-" in den Feldern
         erlaubt. Bei einer invaliden Eingabe, wird das Feld rot markiert, bis
         der Fehler korrigiert wurde.
-      </li>
-    </ul>
+      </p>
+      <img
+        class="max-w-[50%] pl-4"
+        src="../assets/helpcenter/games.webp"
+        alt="Spiel Übersicht"
+      />
+    </div>
     <h3 class="text-3xl font-normal leading-normal mt-4">
       Profil / Statistiken
     </h3>
-    <ul>
-      <li>
+    <div class="flex py-2">
+      <p class="text-left">
         Unter dem <a href="/profile">Profil</a> kann man seine Benutzerdaten
         anpassen und sieht auch persönliche Statistiken
-      </li>
-      <li>
+        <br />
         Der "Benutzername" lässt sich nicht anpassen, nachdem er inital gesetzt
         wurde
-      </li>
-      <li>
+        <br />
         Die Statistiken zeigen in der Übersicht generelle Statistiken an wie
         z.B. Anzahl Spiele, Siegesrate und durchschnittliche Punktzahl pro
         Spiel. Man kann mit einem klick auf die Statistiken die Detail Ansicht
         öffnen. Dort sieht man genauere Informationen auch visuell dargestellt
         und kann dieese auch beliebig filtern.
-      </li>
-    </ul>
-    <p>
+      </p>
+    </div>
+    <p class="py-8">
       Jasskarten-Icons bereitgestellt von Wikimedia Commons User Jensche,
       lizensiert unter
       <a
