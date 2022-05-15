6932a3b99625300bde4440071a640358e914e4a5
Marcel Joss <marcel.joss@ost.ch>
2022-05-15 15:11:08 +0200

    feat(game preview): display current team scores

--- Frontend/src/components/GamePreview.vue
+++ Frontend/src/components/GamePreview.vue
@@ -1,28 +1,46 @@
 <script setup lang="ts">
-import { Game } from "@/types/types";
+import type { Game, Team } from "@/types/types";
+import { useContractStore } from "@/store/contract-store";
+import { computed } from "vue";
 
-defineProps<{ game: Game }>();
+const props = defineProps<{ game: Game }>();
+
+const contractStore = useContractStore();
+
+const teams = computed(() => [props.game.team1, props.game.team2]);
+
+function getTeamScore(team: Team): number {
+  const playerIds = [team.player1.playerId, team.player2.playerId];
+  return props.game.rounds
+    .filter(({ playerId }) => playerIds.includes(playerId))
+    .map((r) => contractStore.getCalculatedScore(r))
+    .reduce((a, b) => a + b, 0);
+}
 </script>
 <template>
   <table>
     <tr>
-      <th>Team 1</th>
-      <th>Team 2</th>
+      <th class="px-2" v-for="(team, index) in teams" :key="index">
+        <div>Team {{ index + 1 }}</div>
+        <div>{{ getTeamScore(team) }} Punkte</div>
+      </th>
     </tr>
     <tr>
-      <td class="border-t-2 border-slate-300">
-        {{ game.team1.player1.displayName }}
-      </td>
-      <td class="border-t-2 border-slate-300">
-        {{ game.team2.player1.displayName }}
+      <td
+        class="border-t-2 border-slate-300"
+        v-for="(team, index) in teams"
+        :key="index"
+      >
+        {{ team.player1.displayName }}
       </td>
     </tr>
     <tr>
-      <td class="border-t-2 border-slate-300">
-        {{ game.team1.player2.displayName }}
-      </td>
-      <td class="border-t-2 border-slate-300">
-        {{ game.team2.player2.displayName }}
+      <td
+        class="border-t-2 border-slate-300"
+        v-for="(team, index) in teams"
+        :key="index"
+      >
+        {{ team.player2.displayName }}
       </td>
     </tr>
   </table>
