78eb49743ede0a44cdff773ed957134e1eb1d571
David Kalchofner <david.kalchofner@ost.ch>
2022-05-27 14:33:07 +0200

    feat(toaster): setup vue toaster
    can be used locally now in components/views

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -2,9 +2,18 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import Toast, { PluginOptions, POSITION } from "vue-toastification";
 
+import "vue-toastification/dist/index.css";
 import "./styles/tailwind.scss";
 import "./styles/custom.scss";
 import "./styles/modal.scss";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+const options: PluginOptions = {
+  position: POSITION.BOTTOM_CENTER,
+  closeOnClick: true,
+  pauseOnHover: false,
+  timeout: 5000,
+};
+
+createApp(App).use(createPinia()).use(router).use(Toast, options).mount("#app");
